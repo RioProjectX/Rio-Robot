8d31d30327bb96b192aa9820dc675b324253dd57
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2022-05-18 02:05:40 +0700

    Update myinfo.py

--- FallenRobot/modules/myinfo.py
+++ FallenRobot/modules/myinfo.py
@@ -17,11 +17,11 @@ from FallenRobot import dispatcher
 
 edit_time = 5
 """ =======================FALLEN ROBOT====================== """
-file1 = "https://telegra.ph/file/9a85d0a873e2dd80d278d.jpg"
-file2 = "https://telegra.ph/file/9e7815284031452afa9e5.jpg"
-file3 = "https://telegra.ph/file/dcc5e003287f69acea368.jpg"
-file4 = "https://telegra.ph/file/ed1ce7fee94f46b0f671e.jpg"
-file5 = "https://telegra.ph/file/701028ce085ecfa961a36.jpg"
+file1 = "https://telegra.ph/file/660176f499df4e28ebc58.jpg"
+file2 = "https://telegra.ph/file/660176f499df4e28ebc58.jpg"
+file3 = "https://telegra.ph/file/660176f499df4e28ebc58.jpg"
+file4 = "https://telegra.ph/file/660176f499df4e28ebc58.jpg"
+file5 = "https://telegra.ph/file/660176f499df4e28ebc58.jpg"
 """ =======================FALLEN ROBOT====================== """
 
 
@@ -65,7 +65,7 @@ async def callback_query_handler(event):
   try:
     boy = event.sender_id
     PRO = await bot.get_entity(boy)
-    LILIE = "POWERED BY 𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦 \n\n"
+    LILIE = "POWERED BY RIO  \n\n"
     LILIE += f"FIRST NAME : {PRO.first_name} \n"
     LILIE += f"LAST NAME : {PRO.last_name}\n"
     LILIE += f"YOU BOT : {PRO.bot} \n"
